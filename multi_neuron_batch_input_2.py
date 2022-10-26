e067eeddccae77d2fb51a35bf8a7b8eecd15eb51
HandikaDio <90175137+HandikaDio@users.noreply.github.com>
2022-10-26 19:58:18 +0700

    Add files via upload

---
+++ multi_neuron_batch_input_2.py
@@ -0,0 +1,44 @@
+# memanggil library numpy
+import numpy as np
+
+# inisialisasi input berdasarkan jumlah batch yaitu 6 dan panjang sesuai input layer yaitu 10
+inputs = [
+    [2.5, 2.5, 3, 1.5, 3.5, 2.5, 4, 3.5, 5.5, 5.6],
+    [2.5, 3.4, 1.2, 2.4, 4.2, 2.4, 5.2, 3.4, 4.2, 6.4],
+    [3.7, 14.5, 14, 15.5, 30.5, 20.5, 40.5, 30.5, 50.5, 60.5],
+    [4.7, 2.8, 3.6, 2.5, 3.5, 4.8, 4.8, 5.8, 5, 6.8],
+    [5.5, 7.4, 7.4, 7.5, 9.2, 7.4, 5.2, 7.4, 10.5, 8.4],
+    [11.5, 15.4, 17.3, 15.4, 19.2, 18.4, 20.2, 20.4, 10.5, 10.4],
+]
+# Hidden layer 1
+# inisialisasi weight berdasarkan jumlah neuron yaitu 5 dan panjang sesuai input layer yaitu 10
+weights = [
+    [2, 2.5, 1, 2.6, 3.5, 3.6, 4.5, 4.2, 6, 4.5],
+    [2.5, 2.4, 3.2, 3.4, 3.5, 2.4, 3.2, 4.5, 6.2, 6.4],
+    [3.7, 2.8, 2.5, 3.8, 3.5, 4.8, 4.5, 5.8, 5.7, 5.9],
+    [3.5, 6.5, 8.2, 7.5, 9.2, 8.5, 9.5, 9.5, 10.5, 10.2],
+    [3.6, 19.5, 19, 20.7, 30.6, 20.5, 40.5, 20.5, 40, 40.5]
+]
+# inisialisasi bias hidden layer 1
+# inisialisasi bias berdasarkan panjang neuron = 5
+biases = [2.5, 2.2, 4.1, 5.7, 5]
+
+# hidden layer 2
+# inisialisasi bobot hidden layer berdasarkan jumlah neuron yaitu 3 dan panjang sesuai input layer yaitu 5
+weights2 = [
+    [-2, 2.1, 2.5, 1, 1.5],
+    [-1, 2, 0.5, 1.5, 2],
+    [1.5, 3, 2, 1, 2.1]
+]
+# inisialisasi bias hidden layer 2
+# inisialisasi bias berdasarkan panjang neuron = 3
+biases2 = [2, 1.5, 3]
+
+# menghitung layer1 menggunakan inputs, weights1, dan biases1
+layer1_outputs = np.dot(inputs, np.array(weights).T) + biases
+
+# menghitung layer2 dari hasil perhitungan layer1
+layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + biases2
+
+# print output layer2
+print(layer2_outputs)
